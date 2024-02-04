7bb1ae5c4767ac461b1aa1d50207a9762802c00c
GuardianN06 <ttdrandom@gmail.com>
2024-02-04 21:33:47 +0100

    Add shit

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -2,18 +2,22 @@ $(document).ready(function () {
     $('#darklight').change(function () {
         if ($(this).prop('checked')) {
             /*LIGHT MODE*/
-            $('body').css('background-color', '#f4f4f5');
+           // $('body').css('background-color', '#f4f4f5');
+            $(':root').css('--backgroundMain', '#f4f4f5');
             $('.navbar__brand').css('color', '#0e1111');
             $('.ioo').css('color', '#14121B');
             $('.active').css('color', '#0e1111');
             $(':root').css('--circleColor', '#73737326');
+            $(':root').css('--backgroundSecond', '#F9F9F9');
         } else {
             /*DARK MODE*/
-            $('body').css('background-color', '#14121B');
+            //$('body').css('background-color', '#14121B');
+            $(':root').css('--backgroundMain', '#14121B');
             $('.navbar__brand').css('color', '#fff');       
             $('.ioo').css('color', '#fff');       
             $('.active').css('color', '#fff');       
             $(':root').css('--circleColor', '#FFFFFF26');       
+            $(':root').css('--backgroundSecond', '#181621');       
         }
     });
 });
